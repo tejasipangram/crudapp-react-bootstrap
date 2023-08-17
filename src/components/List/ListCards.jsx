226b90c4fd5ff75fa9d7ca224058654c4d3b68cc
tejasipangram <tejasgiri@ipangram.com>
2023-08-17 13:31:37 +0530

    show and hide

--- src/components/List/ListCards.jsx
+++ src/components/List/ListCards.jsx
@@ -1,16 +1,45 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import EditList from "./EditList";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../../GloblaCotext";
 
 function ListCard({ title, body, id }) {
+  const [showMore, setShowMore] = useState(false);
   const { deleteList } = useContext(GlobalContext);
   return (
     <Card style={{ width: "25rem", marginTop: "1rem" }}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>{body}</Card.Text>
+        <Card.Text>
+          {showMore ? (
+            <div>
+              {body}
+              <a
+                role="button"
+                className="text-blue cursur-pointer mx-2"
+                onClick={() => {
+                  setShowMore(!showMore);
+                }}
+              >
+                Hide
+              </a>
+            </div>
+          ) : (
+            <div>
+              {body.slice(0, 50)}
+              <a
+                role="button"
+                className="text-blue mx-2"
+                onClick={() => {
+                  setShowMore(!showMore);
+                }}
+              >
+                More
+              </a>
+            </div>
+          )}
+        </Card.Text>
         <EditList id={id} title={title} body={body} name="EDIT" />
         <Button
           onClick={() => {
